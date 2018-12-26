eb8936be16b24251dabfbb3caee3bf5e888c5dac
alex-ht <hung_alex@icloud.com>
2018-12-26 18:09:20 +0800

    number of jobs

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --memory-swap 0 -t wave2letter:cblas .
+docker build --build-arg NJ=40 --memory-swap 0 -t wave2letter:cblas .
 docker tag wave2letter:cblas alexht/wav2letter:cblas
 docker push alexht/wav2letter:cblas
